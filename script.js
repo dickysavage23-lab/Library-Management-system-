afd7b4796e2033895814c88947c189bf5358ff76
dickysavage23-lab <dickysavage23@gmail.com>
2026-08-08 13:36:10 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const searchInput = document.querySelector("input");
+const searchButton = document.querySelector("button");
+
+searchButton.addEventListener("click", function () {
+    const searchText = searchInput.value.toLowerCase().trim();
+
+    if (searchText === "") {
+        alert("Please enter a book name.");
+        return;
+    }
+
+    if (searchText.includes("things fall apart")) {
+        alert("Things Fall Apart - Available");
+    } 
+    else if (searchText.includes("great gatsby")) {
+        alert("The Great Gatsby - Available");
+    } 
+    else if (searchText.includes("1984")) {
+        alert("1984 - Borrowed");
+    } 
+    else {
+        alert("Sorry, book not found.");
+    }
+});
